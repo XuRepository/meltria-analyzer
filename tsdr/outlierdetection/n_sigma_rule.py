3d70e8d27078491d0b077924d722b1c72ea90fac
yuuki <yuki.tsubo@gmail.com>
2022-09-06 17:59:11 +0900

    tsdr: apply coding style

--- tsdr/outlierdetection/n_sigma_rule.py
+++ tsdr/outlierdetection/n_sigma_rule.py
@@ -1,10 +1,14 @@
 import numpy as np
+import pandas as pd
 import scipy.stats
 
 
-def detect_with_n_sigma_rule(x: np.ndarray, test_start_time: int, sigma_threshold=3) -> np.ndarray:
-    """ Detect outliers 'robust z-score' and n-sigma rule.
-    """
+def detect_with_n_sigma_rule(
+    x: np.ndarray | pd.Series,
+    test_start_time: int,
+    sigma_threshold: int = 3,
+) -> np.ndarray:
+    """ Detect outliers 'robust z-score' and n-sigma rule. """
     train, test = np.split(x, [test_start_time])
     coeff = scipy.stats.norm.ppf(0.75)-scipy.stats.norm.ppf(0.25)
     iqr = np.quantile(train, 0.75) - np.quantile(train, 0.25)

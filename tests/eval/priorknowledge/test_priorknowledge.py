b35ad16ec2de9021fa52ed99582c4316514d552e
yuuki <yuki.tsubo@gmail.com>
2022-09-08 14:17:05 +0900

    tests: fix test failure

--- tests/eval/priorknowledge/test_priorknowledge.py
+++ tests/eval/priorknowledge/test_priorknowledge.py
@@ -2,7 +2,7 @@ from meltria.priorknowledge.priorknowledge import new_knowledge
 
 
 def test_generate_service_to_service_routes():
-    pk = new_knowledge("train-ticket")
+    pk = new_knowledge("train-ticket", mappings={})
 
     res = pk.get_service_routes("ts-route-plan")
     assert res == [("ts-travel-plan", "ts-ui-dashboard"), ("ts-ui-dashboard",)]

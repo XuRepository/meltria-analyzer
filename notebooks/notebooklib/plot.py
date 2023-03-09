2d2eb834c6acd1820b31b017bb3de2b308d69d65
yuuki <yuki.tsubo@gmail.com>
2023-03-09 19:08:01 +0900

    notebooks: adjust layout of plotting causal graph

--- notebooks/notebooklib/plot.py
+++ notebooks/notebooklib/plot.py
@@ -2,11 +2,18 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 
-def plot_causal_graph(G: nx.Graph) -> None:
-    plt.figure(figsize=(20, 20))
-    pos = nx.kamada_kawai_layout(G)
-    nx.draw_networkx(G, pos=pos, font_size=8)
-    # nx.set_edge_attributes(g, {(e[0], e[1]): {'label': e[2]['weight']} for e in g.edges(data=True)})
-    labels = {k: round(v, 2) for k, v in nx.get_edge_attributes(G, "weight").items()}
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, font_size=6)
+def plot_causal_graph(
+    G: nx.Graph, figsize: tuple[float, float] = (20, 20), layout_scale: int = 1, node_label_y_off: float = 0.01
+) -> None:
+    plt.figure(figsize=figsize)
+
+    pos = nx.kamada_kawai_layout(G, scale=layout_scale)
+    nx.draw_networkx(G, pos=pos, with_labels=False)
+
+    node_labels_pos = {k: (v[0], v[1] - node_label_y_off) for k, v in pos.items()}
+    nx.draw_networkx_labels(G, pos=node_labels_pos, font_size=8)
+
+    edge_labels = {k: round(w, 2) for k, w in nx.get_edge_attributes(G, "weight").items()}
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=6, label_pos=0.25)
+
     plt.show()

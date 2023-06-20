8338b01db91ecda9efccd657f2510d8242836c65
yuuki <yuki.tsubo@gmail.com>
2023-06-20 22:55:35 +0900

    tsdr: add robust zscore

--- tsdr/outlierdetection/n_sigma_rule.py
+++ tsdr/outlierdetection/n_sigma_rule.py
@@ -35,14 +35,14 @@ def zscore_nsigma(train: np.ndarray, test: np.ndarray, n_sigmas: float = 3.0) ->
     return anomalies, scores
 
 
-def zscore_nsigma_cause_rank(train: np.ndarray, test: np.ndarray, k: float) -> tuple[bool, np.ndarray]:
+def zscore_nsigma_causerank(train: np.ndarray, test: np.ndarray, n_sigmas: float) -> tuple[bool, np.ndarray]:
     mu, sigma = np.mean(train), np.std(train)
     if sigma == 0.0:
         sigma = 0.0001
     scores = np.abs((test - mu) / sigma)
     s_x: float = np.max(scores, axis=0)
-    alpha_x: float = 0 if s_x < k else np.log1p(s_x)
-    return alpha_x > 0, np.log1p(scores)
+    alpha_x: float = 0 if s_x < n_sigmas else np.log1p(s_x)
+    return alpha_x > 0, scores
 
 
 COEFF = scipy.stats.norm.ppf(0.75) - scipy.stats.norm.ppf(0.25)
@@ -55,6 +55,17 @@ def robust_zscore_nsigma(train: np.ndarray, test: np.ndarray, n_sigmas: float = 
     scores = np.abs(test - median)
     anomalies = np.argwhere(scores > niqr * n_sigmas)
     return anomalies, scores
+
+
+def robust_zscore_nsigma_causerank(train: np.ndarray, test: np.ndarray, n_sigmas: float) -> tuple[bool, np.ndarray]:
+    iqr = np.quantile(train, 0.75) - np.quantile(train, 0.25)
+    niqr = iqr / COEFF
+    median = np.median(train)
+    scores = np.abs(test - median)
+    s_x: float = np.max(scores, axis=0)
+    alpha_x: float = 0 if s_x < niqr * n_sigmas else np.log1p(s_x)
+    return alpha_x > 0, scores
+
 
 
 def detect_anomalies_with_zscore_nsigma(
@@ -73,11 +84,12 @@ def detect_anomalies_with_zscore_nsigma(
     return (alarms, scores) if return_score else (alarms.size > 0, scores.max())
 
 
-def detect_anomalies_with_zscore_nsigma_cause_rank(
+def detect_anomalies_with_zscore_nsigma_causerank(
     x: np.ndarray,
     anomalous_start_idx: int,
     n_sigmas: float = 1.0,
+    robust: bool = False,
 ) -> tuple[bool, np.ndarray]:
     test_start_idx = x.shape[0] - (anomalous_start_idx + 1)
     train, test = x[:test_start_idx], x[test_start_idx:]
-    return zscore_nsigma_cause_rank(train, test, k=n_sigmas)
+    return robust_zscore_nsigma_causerank(train, test, n_sigmas) if robust else zscore_nsigma_causerank(train, test, n_sigmas)

4a26e24a3bebaa94cc604bc40ed240fa93d58346
yuuki <yuki.tsubo@gmail.com>
2023-09-03 15:23:52 +0900

    eval: add config for tsifter only

--- eval/config/sweep_tsdr/sockshop_tsifter.conf.py
+++ eval/config/sweep_tsdr/sockshop_tsifter.conf.py
@@ -1,10 +1,9 @@
 from eval.config.common.tsifter_best_params import TSIFTER_BEST_PARAM
 
 CONFIG = dict(
-    dataset_id="m9dgg",
+    dataset_id="9n6mf",
     list_of_tsdr_options=[TSIFTER_BEST_PARAM],
     use_manually_selected_metrics=[True, False],
     progress=True,
-    max_chaos_case_num=3,
-    sampling_scale_factors=[1, 2, 3, 4],
+    max_chaos_case_num=5,
 )
--- eval/config/sweep_tsdr/trainticket_tsifter.conf.py
+++ eval/config/sweep_tsdr/trainticket_tsifter.conf.py
@@ -1,8 +1,8 @@
-from eval.config.common.tsifter_params_sensitivity import TSDR_OPTIONS
+from eval.config.common.tsifter_best_params import TSIFTER_BEST_PARAM
 
 CONFIG = dict(
     dataset_id="m9dgg",
-    list_of_tsdr_options=TSDR_OPTIONS,
+    list_of_tsdr_options=[TSIFTER_BEST_PARAM],
     use_manually_selected_metrics=[True, False],
     progress=True,
     max_chaos_case_num=3,

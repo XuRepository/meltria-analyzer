0ce4e8f7487cb7a82f7ec69a711c4c14da37bb01
yuuki <yuki.tsubo@gmail.com>
2023-09-10 14:18:20 +0900

    eval: add kde_bandwidth_adjust to config

--- eval/config/common/tsifter_params_sensitivity.py
+++ eval/config/common/tsifter_params_sensitivity.py
@@ -1,10 +1,11 @@
 from itertools import product
 from typing import Any
 
+COST_MODELS = ["l2", "normal"]
 SEARCH_METHODS = ["binseg", "pelt", "bottomup"]
 CHANGE_POINTS_PENALTIES = ["bic", "aic"]
-KDE_BANDWIDTHS = ["scott", "silverman", 1.0, 0.5, 0.1]
-COST_MODELS = ["l2", "normal"]
+KDE_BANDWIDTHS = ["scott", "silverman"]
+KDE_BANDWIDTH_ADJUSTS = [0.05, 0.1, 0.25, 0.5, 0.75, 1.0]
 
 TSDR_OPTIONS: list[dict[str, Any]] = [
     dict(
@@ -16,7 +17,9 @@ TSDR_OPTIONS: list[dict[str, Any]] = [
         step1_changepoint_penalty=penalty,
         step2_clustering_method_name="changepoint-kde",
         step2_changepoint_kde_bandwidth=kde_bandwidth,
+        step2_changepoint_kde_bandwidth_adjust=kde_bandwidth_adjust,
         step2_clustering_granularity="service",
     )
-    for search_method, penalty, kde_bandwidth, cost_model in product(SEARCH_METHODS, CHANGE_POINTS_PENALTIES, KDE_BANDWIDTHS, COST_MODELS)
+    for cost_model, search_method, penalty, kde_bandwidth, kde_bandwidth_adjust
+    in product(COST_MODELS, SEARCH_METHODS, CHANGE_POINTS_PENALTIES, KDE_BANDWIDTHS, KDE_BANDWIDTH_ADJUSTS)
 ]

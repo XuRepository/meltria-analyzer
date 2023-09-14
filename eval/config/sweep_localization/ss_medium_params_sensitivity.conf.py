93b55074c5991d3b913af6d694efc7660d1887a2
yuuki <yuki.tsubo@gmail.com>
2023-09-14 10:17:03 +0900

    eval: add ss_medium_params_sensitivity

---
+++ eval/config/sweep_localization/ss_medium_params_sensitivity.conf.py
@@ -0,0 +1,22 @@
+from eval.config.common.tsifter_params_sensitivity import TSDR_OPTIONS
+from eval.config.sweep_localization.common.localizations import \
+    get_list_of_diag_options
+
+CONFIG = dict(
+    dataset_id="9n6mf",
+    n=10,
+    experiment_n_workers=-1,
+    target_chaos_types={"pod-cpu-hog", "pod-memory-hog"},  # "pod-network-latency"},
+    list_of_tsdr_options=TSDR_OPTIONS,
+    list_of_diag_options=get_list_of_diag_options(),
+    pair_of_use_manually_selected_metrics=[False],
+    metric_types_pairs=[{
+        "services": True,
+        "containers": True,
+        "middlewares": False,
+        "nodes": False,
+    }],
+    progress=True,
+    timeout_sec=60 * 60 * 1,
+    # from_orig=(True, 180, 20, 3),
+)
